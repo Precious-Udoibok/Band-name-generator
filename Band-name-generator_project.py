366e1f9cb017a020cc514ff221fa5e2a81cd894d
precious <preciousudoibok@gmail.com>
2024-05-13 14:37:59 +0100

    This is a band name generator using python

---
+++ Band-name-generator_project.py
@@ -0,0 +1,7 @@
+#program to generate a band name
+print('Welcome to the Band Name Generator.')
+city = input("What's the name of the city you grew up in? ") #user enters the city
+pet = input('What is your pet name?') #user enters their pet
+band_name = city + ' ' +  pet #concatenating the city and pet to give the band name
+print('Your band name could be ' + band_name) 
+
